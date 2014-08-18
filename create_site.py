7fadab0021b823d204e806ccadc781509a6bc0e4
Niko Wenselowski <der@nik0.de>
2014-08-18 22:07:16 +0200

    Script to generate the index file from index.html.template

---
+++ create_site.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import datetime
+
+SHOWN_DATES = 3
+
+
+def main():
+    def format_date(date, hide_agenda_link=False):
+        if hide_agenda_link:
+            line_template = '<li><span style="color: #999999;">Mittwoch, {date.day} {month} {date.year} um 19:30 Uhr <!--(<a href="https://pads.ccc.de/pystada-{date.year}-{date.month}-{date.day}">Agenda</a>)--></span></li>'
+        else:
+            line_template = '<li><span>Mittwoch, {date.day} {month} {date.year} um 19:30 Uhr (<a href="https://pads.ccc.de/pystada-{date.year}-{date.month}-{date.day}">Agenda</a>)</span></li>'
+
+        return line_template.format(date=date, month=date.strftime('%B'))
+
+    starting_date = datetime.datetime(2014, 8, 27, 19, 30)
+    delta = datetime.timedelta(weeks=2)
+
+    list_template = '''\
+<ul>
+{elements}
+</ul>\
+'''.format(elements='\n'.join([format_date(starting_date + (i * delta), hide_agenda_link=i) for i in range(SHOWN_DATES)]))
+
+    with open('index.html.template') as template_file:
+        site_template = ''.join(template_file.readlines())
+
+    with open('index.html', 'w') as outputfile:
+        outputfile.write(site_template.format(dates=list_template))
+
+
+if __name__ == '__main__':
+    main()
