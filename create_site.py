66f1ab28accc687f899d8a4edad1c24f9aa4d5a4
Niko Wenselowski <der@nik0.de>
2014-08-18 22:34:39 +0200

    Leading zeroes for the pad links.

--- create_site.py
+++ create_site.py
@@ -9,9 +9,9 @@ SHOWN_DATES = 3
 def main():
     def format_date(date, hide_agenda_link=False):
         if hide_agenda_link:
-            line_template = '<li><span style="color: #999999;">Mittwoch, {date.day} {month} {date.year} um 19:30 Uhr <!--(<a href="https://pads.ccc.de/pystada-{date.year}-{date.month}-{date.day}">Agenda</a>)--></span></li>'
+            line_template = '<li><span style="color: #999999;">Mittwoch, {date.day} {month} {date.year} um 19:30 Uhr <!--(<a href="https://pads.ccc.de/pystada-{date.year}-{date.month:02}-{date.day:02}">Agenda</a>)--></span></li>'
         else:
-            line_template = '<li><span>Mittwoch, {date.day} {month} {date.year} um 19:30 Uhr (<a href="https://pads.ccc.de/pystada-{date.year}-{date.month}-{date.day}">Agenda</a>)</span></li>'
+            line_template = '<li><span>Mittwoch, {date.day} {month} {date.year} um 19:30 Uhr (<a href="https://pads.ccc.de/pystada-{date.year}-{date.month:02}-{date.day:02}">Agenda</a>)</span></li>'
 
         return line_template.format(date=date, month=date.strftime('%B'))
 
