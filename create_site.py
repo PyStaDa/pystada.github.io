6ab1c86831ed7091191e60be9fc8d77e51914d94
Felix Breidenstein <mail@felixbreidenstein.de>
2014-11-19 22:18:55 +0100

    Added an id for the current event

--- create_site.py
+++ create_site.py
@@ -11,7 +11,7 @@ def main():
         if hide_agenda_link:
             line_template = '<li><span style="color: #999999;">Mittwoch, {date.day}. {month} {date.year} um 19:30 Uhr <!--(<a href="https://pads.ccc.de/pystada-{date.year}-{date.month:02}-{date.day:02}">Agenda</a>)--></span></li>'
         else:
-            line_template = '<li><span>Mittwoch, {date.day}. {month} {date.year} um 19:30 Uhr (<a href="https://pads.ccc.de/pystada-{date.year}-{date.month:02}-{date.day:02}">Agenda</a>)</span></li>'
+            line_template = '<li id="current"><span>Mittwoch, {date.day}. {month} {date.year} um 19:30 Uhr (<a href="https://pads.ccc.de/pystada-{date.year}-{date.month:02}-{date.day:02}">Agenda</a>)</span></li>'
 
         return line_template.format(date=date, month=date.strftime('%B'))
 
